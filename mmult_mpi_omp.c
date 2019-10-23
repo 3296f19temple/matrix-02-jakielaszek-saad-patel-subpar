4648169aafdbeee7a267cb574f38c5002994e826
Dishant Patel <tug02945@temple.edu>
2019-10-22 21:55:50 -0400

    Implementation of Automation

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -29,8 +29,9 @@ int main(int argc, char* argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  for(int i = 1; i <= 10; i++){
   if (argc > 1) {
-    nrows = atoi(argv[1]);
+    nrows = atoi(argv[1])*i;
     ncols = nrows;
     if (myid == 0) {
       // Master Code goes here
@@ -51,5 +52,6 @@ int main(int argc, char* argv[])
     fprintf(stderr, "Usage matrix_times_vector <size>\n");
   }
   MPI_Finalize();
+  }
   return 0;
 }
