fb808d102733ffccd140cda7a8647dc8ab75cf86
Dishant Patel <tug02945@temple.edu>
2019-10-22 23:26:44 -0400

    streamlined and cleaned up bash script

--- automate.sh
+++ automate.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 make
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 2
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 3
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 4
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 5
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 6
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 7
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 8
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 9
-mpiexec -f ~/hosts -n 4 ./mmult_mpi_omp 10
+for(n = 2: n<11: n++)
+do
+mpiexec -f ~/hosts -n 4 ../mmult_mpi_omp $n
+done
 make clean
 echo All Done
+exit
